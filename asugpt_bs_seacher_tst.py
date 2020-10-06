578d416e05ef37917776283225dfc58a26fa7794
twmd <slava.sazanov@gmail.com>
2020-10-06 18:11:49 +0300

    function search_log_files

--- asugpt_bs_seacher_tst.py
+++ asugpt_bs_seacher_tst.py
@@ -1,5 +1,13 @@
 import os
-for root, dirs, files in os.walk("/opt"):
-    for file in files:
-        if file.endswith("*.log")
-            print(os.path.join(root, file))
+
+def search_log_files():
+    log_files_list = []
+    for root, dirs, files in os.walk("/opt"):
+        for file in files:
+            if file.endswith("*.log"):
+                #print(os.path.join(root, file))
+                log_files_list.append(os.path.join(root, file))
+        print(log_files_list)
+
+if __name__ == '__main__':
+    search_log_files()
