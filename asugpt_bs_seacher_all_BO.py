5f2af49c67507be73ab9b95f5c242d4600154185
Vyacheslav Sazanov <slava.sazanov@gmail.com>
2020-10-09 12:34:45 +0300

    asugpt_bs_seacher_all_BO.py change for find in .log file only

--- asugpt_bs_seacher_all_BO.py
+++ asugpt_bs_seacher_all_BO.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
             cur_uin = re.sub("^\s+|\n|\r|\s+$", '', cur_uin)
             print(cur_uin)
             gprs_control = search_uin_bs_in_files(search_log_files(), cur_uin)
-            with open ('report_uin.txt', 'w', encoding='UTF-8') as f_report:
+            with open ('report_uin.txt', 'a', encoding='UTF-8') as f_report:
                 cur_time = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
                 f_report.write('{0} | {1} | {2}'.format(str(cur_time), cur_uin, gprs_control))
                 print('{0} | {1} | {2}'.format(str(cur_time), cur_uin, gprs_control))
